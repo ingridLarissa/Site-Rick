e5f76df34d306c54c96db0ff8ee6908e5ffe893d
ingridLarissa <48722564+ingridLarissa@users.noreply.github.com>
2024-03-21 13:12:03 -0300

    servicos.php

---
+++ servicos.php
@@ -0,0 +1,133 @@
+
+<html lang="en">
+<head>
+  <title>Site Henrique</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <style>
+  .fakeimg {
+    height: 200px;
+    *//background: #aaa;//
+  }
+  </style>
+</head>
+<body>
+
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <h1>CONCESSIONÁRIA</h1>
+  <p>Henrique Jeferson</p> 
+</div>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <a class="navbar-brand" href="index.php">INICIO</a>
+   <a class="navbar-brand" href="contato.php">LOCALIZAÇÃO</a>
+   
+  </nav>
+  
+ <div style="display:inline-block;width:33%" align="left"><h5>Prisma</h5>
+      <div class="fakeimg"><img src="prisma.jpg" width="300px" height="200px"></div>
+      <p>O Prisma é um modelo de sedan compacto da Chevrolet cuja primeira geração foi lançada com base no Chevrolet Celta. As gerações seguintes, contudo, passaram a ter como base o Chevrolet Onix, sendo que a terceira geração tem como nome alternativo Onix Plus.
+      </p>
+      </div>
+      
+<div style="display:inline-block;width:33%" align="center"> <h5>Siena</h5>
+		<div class="fakeimg"><img src="siena.jpg" width="300px" height="200px"></div>
+		 <p>O Siena foi montado com base no Fiat Palio com sua primeira versão ainda em 1997. O carro desde então passou por 4 facelifts e ainda ganhou uma segunda geração (o Grand Siena) que ainda está no mercado. 
+As primeiras versões não agradaram muito ao mercado, mas com os facelifts e as atualizações em potência e design, o carro começou a ganhar o mercado e as ruas brasileiras. 
+	</p>
+	 </div>
+<div style="display:inline-block;width:33%" align="right"><h5>Corolla</h5>
+		<div class="fakeimg"><img src="corolla.jpg" width="300px" height="200px"></div>
+		<p> O Corolla é um dos sedans mais tradicionais do mundo - atualmente, chega a 12 gerações! O carro da montadora japonesa Toyota é considerado extremamente econômico, fazendo cerca de 10km/l dentro da cidade e até 15km/l na estrada.
+Além disso, possui bom espaço interno, acabamento de qualidade e um design moderno e elegante. 
+</p></div>
+<div class="container" style="margin-top:30px">
+  <div class="row">
+     
+      <hr class="d-sm-none">
+</div>
+</div>
+
+
+       <div style="display:inline-block;width:33%" align="left"><h5>Virtus</h5>
+      <div class="fakeimg"><img src="virtus.jpg" width="300px" height="200px"></div>
+      <p>O Virtus é um sedan da Volkswagen derivado do Polo. O carro conta com 4 versões de acabamento, incluindo uma versão de luxo, uma intermediária de luxo e uma versão esportiva. 
+      </p>
+      </div>
+      
+<div style="display:inline-block;width:33%" align="center"> <h5>Voyage</h5>
+		<div class="fakeimg"><img src="voyage.jpg" width="300px" height="200px"></div>
+		 <p>O Voyage é outro sedan compacto da Volkswagen, sendo este derivado do hatch Gol (um dos carros mais vendidos no país em todos os tempos). No exterior, o veículo em sua primeira geração era chamado de Volkswagen Fox (não confundir com o Fox vendido no Brasil até 2021). 
+	 </p>
+	 </div>
+<div style="display:inline-block;width:33%" align="right"><h5>Etios</h5>
+		<div class="fakeimg"><img src="etios.jpg" width="300px" height="200px"></div>
+		<p> O Etios é um modelo fabricado pela Toyota para os mercados emergentes do mundo que está sendo descontinuado nos mesmos pouco a pouco.
+Seu modelo sedan compacto apresenta as mesmas características e acabamento que a versão hatch, mas conta com um porta-malas de 562l.
+		</p>
+		</div>
+<div class="container" style="margin-top:30px">
+  <div class="row">
+       
+      <hr class="d-sm-none">
+</div>
+</div>
+
+
+
+
+       <div style="display:inline-block;width:33%" align="left"><h5>Civic</h5>
+      <div class="fakeimg"><img src="civic.jpg" width="300px" height="200px"></div>
+      <p>O Civic é outro sedan com grande tradição, sendo fabricado pela Honda desde 1973. O carro na versão atual possui motor 2.0 com 158hp e opção em câmbio automático ou manual de 6 marchas. 
+O Civic tem também versões hatch e coupê, mas a sua mais tradicional atualmente no mercado brasileiro é a sedan. 
+		</p>
+      </div>
+      
+<div style="display:inline-block;width:33%" align="center"> <h5>Cruze</h5>
+		<div class="fakeimg"><img src="cruze.jpg" width="300px" height="200px"></div>
+		 <p>O Cruze é o modelo de sedan médio da Chevrolet para o mercado mundial e apresenta motor 1.8 na primeira geração (140cv gasolina e 144cv etanol) e 1.4 na segunda (150cv gasolina e 153cv etanol). 
+O carro tem câmbio automático e grande espaço interno. Além disso, foi eleito o veículo favorito das mulheres pela revista O Poder Feminino em 2016. 
+Possui uma frente arrojada e é uma excelente opção para quem busca sedans com bom acabamento
+	</p>
+	 </div>
+<div style="display:inline-block;width:33%" align="right"><h5>Versa</h5>
+		<div class="fakeimg"><img src="versa.jpg" width="300px" height="200px"></div>
+		<p> O Nissan Versa foi reestilizado recentemente e, na versão de entrada, agora tem rodas de liga-leve de 15", luzes diurnas em LEDs, sistema multimídia com espelhamento, controles de tração e estabilidade, 6 airbags e o motor 1.6 aspirado ligado ao câmbio CVT.
+		</p>
+		</div>
+<div class="container" style="margin-top:30px">
+  <div class="row">
+       
+      <hr class="d-sm-none">
+</div>
+</div>
+
+
+
+
+       <div style="display:inline-block;width:33%" align="left"><h5>Citroen C3</h5>
+      <div class="fakeimg"><img src="citroen.jpg" width="300px" height="200px"></div>
+      <p>A Citroën está em um processo de renovação, após alguns anos vendendo somente o C4 Cactus. O primeiro carro da nova linha da marca é o C3, que chegou para ser um dos carros mais baratos do mercado. Busca os clientes que até cogitam Kwid ou Mobi, mas com mais espaço. A versão Live vem com ar-condicionado, direção elétrica, controles de estabilidade e tração, assistente de partida em rampas, travas e vidros dianteiros elétricos, monitoramento de pressão dos pneus e rodas de aço de 15” com calotas.
+		</p>      
+      </div>
+      
+<div style="display:inline-block;width:33%" align="center"> <h5>Peugeot 208</h5>
+		<div class="fakeimg"><img src="peugeot.jpg" width="300px" height="200px"></div>
+		 <p>Após a fusão do Grupo PSA com a Fiat-Chrysler, a Stellantis mudou o posicionamento do Peugeot 208 no mercado brasileiro, criando as versões com o motor 1.0 Firefly de 75 cv e 10,7 kgfm compartilhado com Argo e Cronos. Isto ajudou a posicionar o hatch na lista dos 10 carros mais baratos do Brasil e aumentar as vendas do veículo.
+	 	</p>
+	   </div>
+<div style="display:inline-block;width:33%" align="right"><h5>Onix</h5>
+		<div class="fakeimg"><img src="onix.jpg" width="300px" height="200px"></div>
+		<p> Entre as duas listas, a Chevrolet aposentou o Joy e deixou ao Onix o papel de seu carro de entrada. A versão de entrada não tem um nome específico, mas tem 6 airbags, ar-condicionado, direção elétrica e som com USB e bluetooth. É bem simples visualmente, com rodas de 14" com calotas e sem adesivos nas colunas
+		</p>
+		</div>
+<div class="container" style="margin-top:30px">
+  <div class="row">
+       
+      <hr class="d-sm-none">
+</div>
+</div>
