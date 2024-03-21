46b1f462c83dba767f090db7445c7e1d462f02f9
ingridLarissa <48722564+ingridLarissa@users.noreply.github.com>
2024-03-21 13:10:51 -0300

    contato.php

---
+++ contato.php
@@ -0,0 +1,39 @@
+<html lang="en">
+<head>
+  <title>Site Henrique</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <style>
+  .fakeimg {
+    height: 200px;
+    *//background: #aaa;//
+  }
+  </style>
+</head>
+<body>
+
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <h1>CONCESSIONÁRIA</h1>
+  <p>Henrique Jeferson</p> 
+</div>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <a class="navbar-brand" href="index.php">INICIO</a>
+  <a class="navbar-brand" href="servicos.php">MODELOS DE CARROS</a>
+  </nav>
+<div class="container" style="margin-top:30px">
+  <div class="row">
+    <div class="col-sm-7">
+    <h1> Localização</h1>
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3660.357894413209!2d-51.87183288502417!3d-23.447551484739442!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ecdabcbb262c97%3A0x1286c3f69a9f00db!2sCol%C3%A9gio+Ant%C3%B4nio+Francisco+Lisboa!5e0!3m2!1spt-BR!2sbr!4v1556624232640!5m2!1spt-BR!2sbr" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>  
+		</div>
+    <div class="col-sm-5">
+      <h2>Contato</h2>
+      <p>Henrique Jeferson (44) 0 0000-0000</p> 
+  </div>
+</div>
+
